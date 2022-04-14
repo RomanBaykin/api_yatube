038b0c46cc4794706b1dbc275b3beda8bb71d493
Роман Байкин <baykinroman@gmail.com>
2022-04-14 23:56:39 +0300

    api_yatube

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,5 @@
-from django.core.exceptions import PermissionDenied
 from rest_framework import viewsets
-from rest_framework.decorators import permission_classes
+from .permissions import IsAuthorOrReadOnly
 from rest_framework.permissions import IsAuthenticated
 
 from posts.models import Post, Comment, Group
@@ -8,27 +7,18 @@ from .serializers import PostSerializer, CommentSerializer
 from .serializers import GroupSerializer
 
 
-@permission_classes([IsAuthenticated])
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(PostViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        instance.delete()
-
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
@@ -37,16 +27,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Руки прочь от чужого!')
-        instance.delete()
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
